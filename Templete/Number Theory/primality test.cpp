8b4a0dc7eb105d49284a621889d616a2128df46b
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-05-31 00:50:44 +0600

    Create primality test.cpp

---
+++ Templete/Number Theory/primality test.cpp
@@ -0,0 +1,64 @@
+///primality test O(sqrt(n))
+
+int n;
+cin>>n;
+
+for(int i=2;i*i<=n;i++)
+{
+    if(n%i==0)
+    {
+        cout<<"Not prime";
+        return;
+    }
+}
+cout<<"prime";
+
+return;
+
+
+///Primality test with farmet little theorem O(logn)
+
+///comment the defined mod;
+
+    ll mulmod(ll a,ll b,ll c){
+
+        ll x=0,y=a%c;
+
+        while(b>0){
+            if(b%2==1){
+                x=(x+y)%c;
+            }
+            y=(y*2ll)%c;
+            b/=2;
+        }
+        return x%c;
+    }
+
+    ll Binary_expo(ll a,ll n)
+    {
+        ll res=1;
+        while(n)
+        {
+            if(n&1)
+            res=mulmod(res,a);
+
+            n/=2;
+            a=mulmod(a,a);
+        }
+        return res%MOD;
+    }
+
+    bool farmet_prime_test(ll n,int iterations=5)
+    {
+        if(n<=4)
+        return n==2 || n==3;
+
+        for(int i=1;i<=iterations;i++)
+        {
+            ll a=2+rand()%(n-3);
+            ll res=Binary_expo(a,n-1,n);
+
+            if(res!=1) return false;
+        }
+        return true;
+    }
