01b17e040e0f5177f299ed87a7b1445563aae9c0
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-05-31 00:22:04 +0600

    Update 1.cpp

--- Conversion/1.cpp
+++ Conversion/1.cpp
@@ -1,5 +1,5 @@
 
-///Charecter to Intger
+*******Charecter to Intger****
 
 char ch='7';
 int a=(int)(ch-'0');
@@ -11,5 +11,43 @@ for(int i=0;i<s.size();i++){
      int a=(int)(ch-'0');
      cout<<a<<endl;
 }
+
+******string to integer*********
+
+///builtin function
+
+string s="738642";
+int n=stoi(s);
+
+
+///function
+
+int convert(string s) {
+    int num=0;
+    int n=s.size();
+
+    for(int i=0;i<n;i++)
+        num=num*10+(int(s[i])-48);
+    return num;
+}
+
+********integer to string******
+
+///builtin fumction
+
+int n=214123;
+string s=to_string(n);
+
+
+********integer to charecter*****
+
+string s="";
+int a=5,b=6;
+char ch=((b-a)+'0');
+b+=ch;
+
+or
+b+=(char)((b-a)+'0');
+
 
 
