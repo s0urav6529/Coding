451f70b428cc830d284ead4c04c83b6fae6ce578
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-05-31 14:24:11 +0600

    Create diagonal traversal demo.cpp

---
+++ Templete/diagonal traversal demo.cpp
@@ -0,0 +1,15 @@
+
+for(int gap=1;gap<=n;gap++) ///Diagonal traversal
+{
+    for(int r=1,c=gap;r<=n,c<=n;r++,c++) ///main and upper diagonal
+    {
+        //
+    }
+    if(gap>1)
+    {
+        for(int r=gap,c=1;r<=n;r++,c++) ///lower daigonal
+        {
+            //
+        }
+    }
+}
