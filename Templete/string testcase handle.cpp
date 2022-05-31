a8249ca3a86479217889761f0aa5c0f46919c44f
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-05-31 14:22:55 +0600

    Create string testcase handle.cpp

---
+++ Templete/string testcase handle.cpp
@@ -0,0 +1,19 @@
+
+void solution(int t){
+
+    string s;
+    getline(cin,s);
+
+    return;
+}
+int main()
+{
+    int tc;
+    cin>>tc;
+    cin.get();
+    for(int t=1;t<=tc;t++) {
+        solution(t);
+    }
+    return 0;
+}
+
