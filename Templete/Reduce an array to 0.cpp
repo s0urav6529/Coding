d7786c39ebb3847ac8f0497955990dfd15224cf3
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-08-08 20:59:03 +0600

    Update Reduce an array to 0.cpp

--- Templete/Reduce an array to 0.cpp
+++ Templete/Reduce an array to 0.cpp
@@ -1,1 +1,28 @@
 
+
+ ///Minimum operation needed to make an all the elements 0 .
+
+ /// Operations choose L & R then reduce L to R by 1.
+  
+  for(int i=1;i<=n;i++) {
+      int x;
+      cin>>x;
+      if(x==a.back()) continue;
+      else a.push_back(x);
+  }
+ 
+    a.push_back(0);
+    int len=a.size(),lagbe=0;
+    for(int i=1;i<len-1;i++){
+
+        if(a[i]>a[i-1] && a[i]>a[i+1]){
+ 
+            lagbe+=(a[i]-max(a[i-1],a[i+1]));
+
+            a[i]=max(a[i-1],a[i+1]);
+            i++;
+        }
+    }
+    sortv(a);
+    lagbe+=a.back();
+
