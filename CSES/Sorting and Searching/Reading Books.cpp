d3341021eed666af8a343ad3c64cbeaea16971de
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-08-14 01:48:20 +0600

    Add files via upload

---
+++ CSES/Sorting and Searching/Reading Books.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long int ll;
+const int limit=2e5+5;
+
+
+int main(){
+    fast;
+
+    int n;
+    cin>>n;
+    vector<ll>a(n);
+
+    for(int i=0;i<n;i++) cin>>a[i];
+    sort(a.begin(),a.end());
+
+    ll sum=0;
+
+    for(int i=0;i<n-1;i++) sum+=a[i];
+
+    if(sum<a[n-1]) {
+        cout<<2ll*a[n-1]<<endl;
+    }
+    else cout<<sum+a[n-1]<<endl;
+
+
+    return 0;
+}
+
+
+
