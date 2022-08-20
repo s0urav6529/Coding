4aba517aa1b9a14fc2209b692c10d0641c086a7e
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-08-20 20:26:51 +0600

    Create Triangle Partition.cpp

---
+++ IDTC-2022/Triangle Partition.cpp
@@ -0,0 +1,29 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define Fin freopen ("in.txt","r",stdin);
+#define Fout freopen ("out1.txt","w",stdout);
+typedef long long int ll;
+constexpr ll MOD=1000000007;
+const int limit=2e5+5;
+
+int main(){
+    fast;
+    int tc=1;
+    cin>>tc;
+    while(tc--){
+        ll k;
+        cin>>k;
+        k++;
+        k*=k;
+        cout<<k<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
