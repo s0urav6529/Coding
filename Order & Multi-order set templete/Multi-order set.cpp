e33156396f6069ef7e920c68c66cd46597c1b6aa
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-08-24 19:49:42 +0600

    Update Multi-order set.cpp

--- Order & Multi-order set templete/Multi-order set.cpp
+++ Order & Multi-order set templete/Multi-order set.cpp
@@ -4,14 +4,7 @@ BLOG LINK: https://codeforces.com/blog/entry/88193
 
 #include <ext/pb_ds/tree_policy.hpp>
 #include <ext/pb_ds/assoc_container.hpp>
-
-namespace __gnu_pbds{
-          typedef tree<int,
-                       null_type,
-                       less_equal<int>,
-                       rb_tree_tag,
-                       tree_order_statistics_node_update> ordered_set;
-}
+namespace __gnu_pbds{ typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; }
 using namespace __gnu_pbds;
 
 
