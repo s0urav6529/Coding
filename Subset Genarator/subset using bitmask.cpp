81457aff0d06eee0afd33adb9c8c79c6bd44a6c4
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-08-30 19:47:08 +0600

    Create subset using bitmask.cpp

---
+++ Subset Genarator/subset using bitmask.cpp
@@ -0,0 +1,36 @@
+
+
+#include<bits/stdc++.h>
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int llu;
+constexpr ll mod=1e9+7;
+const int limit=1e2+5;
+
+int main(){
+    fast;
+
+    string s; cin>>s;
+
+    ll n=s.size();
+
+    ll sz=(1<<n);   /// left shifting because we have 2^n combination
+
+    for(int mask=0 ; mask<sz ; mask++){  
+
+        for(int bit=0 ; bit<n ; bit++){   /// bit position charecter include korbo ki korbo na
+
+            if((mask & (1 << bit))) cout<< s[bit] <<" ";
+
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+
+
