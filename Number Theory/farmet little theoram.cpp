4c34771e1ea0487992d75fcce694a1527f3ca243
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-09-03 12:22:13 +0600

    Update farmet little theoram.cpp

--- Number Theory/farmet little theoram.cpp
+++ Number Theory/farmet little theoram.cpp
@@ -4,6 +4,8 @@ Fermat’s little theorem states that if p is a prime number, then for any integ
 Link : https://www.geeksforgeeks.org/fermats-little-theorem/#:~:text=Fermat%27s%20little%20theorem%20states%20that,an%20integer%20multiple%20of%20p.&text=ap%20%E2%89%A1%20a%20(mod,an%20integer%20multiple%20of%20p.
     
 
+>>>>>>>>>>>>> Time Complexity : O ( log m )
+
 #include<bits/stdc++.h>
 using namespace std;
 
