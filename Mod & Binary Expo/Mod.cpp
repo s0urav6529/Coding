cea73b2cbb2e505e1672edc0d55a03ba94a7cf96
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-09-09 02:14:40 +0600

    Update Mod.cpp

--- Mod & Binary Expo/Mod.cpp
+++ Mod & Binary Expo/Mod.cpp
@@ -1,31 +1,39 @@
 
 
-ll addmod(ll a,ll b){
-    return Mod(Mod(a)+Mod(b));  ///((a%m)+(b%m))%m
+ll fact[limit];
+
+fact[0] = 1;
+for(ll i=1;i<limit;i++){
+     fact[i]=mulmod(fact[i-1],i);
 }
 
 ll Mod(ll x){
-    return ((x%MOD + MOD)%MOD); ///we add mod because x can be positive or negative
+    return ((x%MOD + MOD)%MOD);
 }
-
 
 ll mulmod(ll a,ll b){
-    return Mod(Mod(a)*Mod(b)); ///((a%m)*(b%m))%m
+    return Mod(Mod(a)*Mod(b));
 }
 
-ll Binary_expo(ll a,ll p){   ///a^p
+ll Binary_expo(ll a,ll p){
+
 	ll res=1;
 	while(p){
 
 		if(p & 1)
 		res=mulmod(res,a);
-
 		p/=2;
 		a=mulmod(a,a);
 	}
 	return res%MOD;
 }
 
+ll nCr(ll n,ll r){
+
+    ll numerator= fact[n];
+    ll denominator = mulmod(fact[r],fact[n-r]);
+    return mulmod(numerator, Binary_expo(denominator,MOD-2));
+}
 
 
 
