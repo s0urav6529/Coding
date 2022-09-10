7cd2c712613fe04ed3dfa160a727b67a02a1efba
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-09-10 19:41:03 +0600

    Update and rename # of subarray with zero sum.cpp to # of subarray with 0 or K sum.cpp

--- Subarray related Algo/# of subarray with 0 or K sum.cpp
+++ Subarray related Algo/# of subarray with 0 or K sum.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n,sum=0,ans=0;
-    cin>>n;
+    int n , k, sum=0, ans=0;
+    cin>>n >> k;   ///k-> user demand sum otherwise k->0 id  zero sum
 
     map<int,int>mp;
     mp[0]=1;
@@ -10,7 +10,7 @@ int main(){
     for(int i=1;i<=n;i++){
         int x; cin>>x;
         sum+=x;
-        ans+=mp[sum];
+        ans+=mp[sum-k];    //// if zero sum the k=0 otherwise k->user demand
         mp[sum]++;
     }
     cout<<ans<<endl;
