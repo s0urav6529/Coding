c2f3642c47d160e3d32d9cc7fdc3ee8562553a4c
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-09-10 19:41:29 +0600

    Update # of subarray with 0 or K sum.cpp

--- Subarray related Algo/# of subarray with 0 or K sum.cpp
+++ Subarray related Algo/# of subarray with 0 or K sum.cpp
@@ -1,8 +1,10 @@
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
     int n , k, sum=0, ans=0;
-    cin>>n >> k;   ///k-> user demand sum otherwise k->0 id  zero sum
+    cin>>n >> k;   ///k-> user demand sum otherwise k->0 zero sum
 
     map<int,int>mp;
     mp[0]=1;
