39b8551723b4c0f3188715962f957656e1941a13
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-09-13 00:04:25 +0600

    Update order set.cpp

--- Order & Multi-order set templete/order set.cpp
+++ Order & Multi-order set templete/order set.cpp
@@ -4,9 +4,8 @@
 
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 using namespace __gnu_pbds;
-template<typename T>
-using ordered_set=tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
 
 ordered_set s;
 
@@ -16,7 +15,7 @@ s.insert(1);
 
 int x=*(s.find_by_order(k));   ///kth element of the set --- 0 base index;
 
-int x= order_of_key(val);  /// position of the val in the set  --- 0 base index;
+int x= s.order_of_key(val);  /// position of the val in the set  --- 0 base index;
 
 
 
