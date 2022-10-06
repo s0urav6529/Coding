61587bbdface041db1c585c507ef5c7686782e0b
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-10-06 20:16:11 +0600

    Update segment tree maximum value.cpp

--- Segment Tree/segment tree maximum value.cpp
+++ Segment Tree/segment tree maximum value.cpp
@@ -1,64 +1,61 @@
+
+
+/// for building tree nlogn
+/// for each query(Q) logn
+///Time comlexity = (nlogn+(Q*logn))
+
+
 #include<bits/stdc++.h>
 using namespace std;
-using ll=long long;
-using ld=long double;
-#define mod 1000000007
-#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define read freopen ("in.txt","r",stdin);
-#define out freopen ("out.txt","w",stdout);
-#define mxx 100005
-int n;
-int a[mxx];
-int tree[mxx*4];
-void segment_tree(int node,int b,int e)
-{
+typedef long long int ll;
+const int limit=2e5+5;
+
+int a[limit];
+int st[limit << 2];
+
+void segment_tree(int node,int b,int e){
+
     if(b==e){
-        tree[node]=a[b];
+        st[node] = a[b];
         return;
     }
-    int left_c=node*2;
-    int right_c=node*2+1;
+
     int mid=(b+e)/2;
 
-    segment_tree(left_c,b,mid);
-    segment_tree(right_c,mid+1,e);
+    segment_tree(node << 1, b, mid);
+    segment_tree(node << 1 | 1 , mid+1, e);
 
-    tree[node]=max(tree[left_c],tree[right_c]);
+    st[node] = max(st[node << 1],st[node << 1 | 1]);
+
 }
-int mx(int node,int b,int e,int l,int r)
-{
+
+int Maximum_value(int node,int b,int e,int l,int r){
+
     if(e<l || b>r)
-        return -1000;
+        return -1000;///invalid return
 
     if(b>=l && e<=r)
-        return tree[node];
+        return st[node];
 
-    int left_c=node*2;
-    int right_c=node*2+1;
     int mid=(b+e)/2;
 
-    return max(mx(left_c,b,mid,l,r),mx(right_c,mid+1,e,l,r));
+    return max(Maximum_value(node << 1 , b, mid, l, r) , Maximum_value(node << 1 | 1, mid+1, e, l ,r));
 }
 
-int main()
-{
-    fast;
-    int q;
-    cin>>n>>q;
-    for(int i=1;i<=n;i++) //entering the array
-        cin>>a[i];
-    segment_tree(1,1,n);  ///build the segment tree
+int main(){
 
-    while(q--){
+    int q , n;
+    cin>>n >> q;
+
+    for(int i=1;i<=n;i++)  cin>>a[i];
+
+    segment_tree(1,1,n); ///Bulid segment tree
+
+    while(q--) {
         int l,r;
-        cin>>l>>r;
-        cout<<mx(1,1,n,l,r)<<endl;
+        cin>>l >> r; ///range
+        cout<<Maximum_value(1,1,n,l,r)<<endl;
     }
     return 0;
 }
-
-
-
-
-
 
