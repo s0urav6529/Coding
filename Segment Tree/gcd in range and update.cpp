26c46cfa2bbd9a10bc2ff087fd9015588b1a4e2c
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-10-06 20:02:49 +0600

    Update gcd in range and update.cpp

--- Segment Tree/gcd in range and update.cpp
+++ Segment Tree/gcd in range and update.cpp
@@ -7,80 +7,75 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-using ll=long long;
-using ld=long double;
-#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long int ll;
+const int limit=2e5+5;
 
 int n,q;
-ll a[100005];
-ll tree[100005*4];
+ll a[limit];
+ll tree[limit << 2];
 
-void segment_tree(int node,int b,int e)
-{
+void segment_tree(int node,int b,int e){
+
     if(b==e){
         tree[node]=a[b];
         return;
     }
 
-    int left=node*2;
-    int right=node*2+1;
-
     int mid=(b+e)/2;
 
-    segment_tree(left,b,mid);
-    segment_tree(right,mid+1,e);
+    segment_tree(node << 1, b,  mid);
+    segment_tree(node << 1 | 1, mid+1, e);
 
-    tree[node]=__gcd(tree[left],tree[right]);
+    tree[node]=__gcd(tree[node  << 1],tree[node << 1 | 1]);
 }
 
-ll find_gcd(int node,int b,int e,int l,int r)
-{
+ll find_gcd(int node,int b,int e,int l,int r){
+
     if(e<l || b>r)   ///invalid range
         return 0;
 
     if(b>=l && e<=r)   ///Relavent range
         return tree[node];
 
-    int left=node*2;
-    int right=node*2+1;
-
     int mid=(b+e)/2;
 
-    ll GCD=__gcd(find_gcd(left,b,mid,l,r),find_gcd(right,mid+1,e,l,r));
+    ll GCD=__gcd(find_gcd(node << 1, b, mid , l , r),find_gcd(node << 1 | 1, mid+1, e, l, r));
 
     return GCD;
 }
 
-void update_node(int node,int b,int e,int in)
-{
+void update_node(int node,int b,int e,int index){
+
     if(b==e){
-        tree[node]=a[in];
+        tree[node]=a[index];
         return;
     }
-    int left=node*2;
-    int right=node*2+1;
+
     int mid=(b+e)/2;
 
-    if(in<=mid) update_node(left,b,mid,in);
-    else update_node(right,mid+1,e,in);
+    if(index<=mid) update_node(node << 1 ,b ,mid, index);
+    else update_node(node << 1 | 1, mid+1, e, index);
 
-    tree[node]=__gcd(tree[left],tree[right]);
+    tree[node]=__gcd(tree[node << 1],tree[node << 1 | 1]);
 }
 
-int main()
-{
-    fast;
-    cin>>n>>q;
+int main(){
+
+    cin>> n >> q;
     for(int i=1;i<=n;i++) cin>>a[i];
+
     segment_tree(1,1,n); ///build segment tree
+
     while(q--){
         int type;
         cin>>type;
+
         if(type==1){
             int l,r;
             cin>>l>>r;
             cout<<find_gcd(1,1,n,l,r)<<endl;
         }
+
         else{
             int in,val;
             cin>>in>>val;
@@ -90,3 +85,5 @@ int main()
     }
     return 0;
 }
+
+
