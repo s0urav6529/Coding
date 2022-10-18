a146daaf60a30b1407fbe0f26356ef72fdc405a1
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-10-18 14:24:23 +0600

    Update Mod.cpp

--- Mod & Binary Expo & nCr/Mod.cpp
+++ Mod & Binary Expo & nCr/Mod.cpp
@@ -1,11 +1,13 @@
 
-
+/// **** Factorial code **** Time complexity O(n)
 ll fact[limit];
 
 fact[0] = 1;
 for(ll i=1;i<limit;i++){
      fact[i]=mulmod(fact[i-1],i);
 }
+
+/// ************ Bigmod or Binary Exponentaition *********   Time complexity O(log(n))
 
 ll Mod(ll x){
     return ((x%MOD + MOD)%MOD);
@@ -28,15 +30,40 @@ ll Binary_expo(ll a,ll p){
 	return res%MOD;
 }
 
+
+//// ************** Modular Inverse *********** Time complexity O(log(n))
+	
 ll modular_inverse(ll base){
      return Binary_expo(base,MOD-2);	/// base is the denominator
 }
+
+/// ************** Modular inverse in range ********* Time complexity O(n)
+
+///  blog : https://cp-algorithms.com/algebra/module-inverse.html#mod-inv-all-num
+
+ll mod_inv[limit];
+
+void range_mod_inverse(){
+
+    mod_inv[1] = 1;
+
+    for(int i=2;i<limit;i++){
+
+        mod_inv[i] = MOD - 1ll * (MOD/i) * mod_inv[MOD % i] % MOD;
+
+    }
+
+}
+
+
+//// ************  nCr **********************    Time complexity O(log(n))
 
 ll nCr(ll n,ll r){
 
     ll numerator= fact[n];
     ll denominator = mulmod(fact[r],fact[n-r]);
     return mulmod(numerator, Binary_expo(denominator,MOD-2));
+	
 }
 
 
