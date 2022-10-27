523c877d2c5fe745bdceb7b5b7c3febd3f797be3
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-10-27 23:55:30 +0600

    Update J - Another Game With Numbers.cpp

--- Inclusion Exclusion/J - Another Game With Numbers.cpp
+++ Inclusion Exclusion/J - Another Game With Numbers.cpp
@@ -1,31 +1,11 @@
+
+Find the number of integers from [1 - N] are divisible by a set of number[prime or not prime].
+
 #include<bits/stdc++.h>
 using namespace std;
 #define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define read freopen ("in.txt","r",stdin);
-#define out freopen ("out.txt","w",stdout);
-#define reset(a,b)   memset(a, (b), sizeof(a))
-#define sortv(k)     sort(k.begin(),k.end())
-#define sortg(k)     sort(k.begin(),k.end(),greater<int>())
-#define rev(k)       reverse(k.begin(),k.end())
-#define umin(a,b)    a=min(a,b)
-#define umax(a,b)    a=max(a,b)
-#define min3(a,b,c)  min(a, min(b,c))
-#define pyes         cout<<"Yes"<<endl
-#define pno          cout<<"No"<<endl
-#define pf(a)        cout<<a<<endl
-#define p2f(a,b)     cout<<a<<" "<<b<<endl
-#define p3f(a,b,c)  cout<<a<<" "<<b<<" "<<c<<endl
-#define p4f(a,b,c)  cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl
-#define ff first
-#define ss second
-#define pb push_back
-#define pi acos(-1.0)
-//cin.get();
 typedef long long int ll;
-//typedef unsigned long long int llu;
-//constexpr llu Mx=18446744073709551615;
-constexpr ll MOD=1000000007;
-const int limit=2e5+5;
+
 
 void run_case(){
 
@@ -42,6 +22,7 @@ void run_case(){
     for(int mask=1;mask<(1<<k);mask++){
 
         ll N=n,nod=0,lcm=1;
+        
         for(int bit=0;bit<k;bit++){
 
             if((mask & ( 1 << bit ))){
@@ -60,17 +41,10 @@ void run_case(){
         else ans -= N/lcm;
     }
 
-    cout<<n-ans<<endl;
+    cout<<ans<<endl;
 
 
     return;
 }
 
-int main(){
-    fast;
-    int tc=1;
-    //cin>>tc;
-    while(tc--) run_case();
-    return 0;
-}
 
