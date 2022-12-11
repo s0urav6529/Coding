66ff60366ad946d94ef5409cce36fb87d273f5ca
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-12-11 20:51:55 +0600

    Add files via upload

---
+++ CSES/Mathematics/Stair Game.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define read freopen ("in.txt","r",stdin);
+#define out freopen ("out.txt","w",stdout);
+#define reset(a,b)   memset(a, (b), sizeof(a))
+#define sortv(k)     sort(k.begin(),k.end())
+#define sortg(k)     sort(k.begin(),k.end(),greater<int>())
+#define uniq(vec)    vec.resize(distance(vec.begin(),unique(vec.begin(),vec.end())))
+#define rev(k)       reverse(k.begin(),k.end())
+#define umin(a,b)    a=min(a,b)
+#define umax(a,b)    a=max(a,b)
+#define min3(a,b,c)  min(a, min(b,c))
+#define pyes         cout<<"Yes"<<endl
+#define pno          cout<<"No"<<endl
+#define pf(a)        cout<<a<<endl
+#define p2f(a,b)     cout<<a<<" "<<b<<endl
+#define p3f(a,b,c)  cout<<a<<" "<<b<<" "<<c<<endl
+#define p4f(a,b,c)  cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl
+#define ff first
+#define ss second
+#define pb push_back
+#define pi acos(-1.0)
+//cin.get();
+typedef long long int ll;
+//typedef unsigned long long int llu;
+//constexpr llu Mx=18446744073709551615;
+constexpr ll MOD=1000000007;
+const int limit=1e6+5;
+
+
+void an1nd1ta(int t){
+
+    int n;
+    cin >> n;
+
+    ll Xor = 0;
+
+    for(int i=0;i<n;i++){
+        ll x;
+        cin >> x;
+
+        if(i&1){
+            Xor^=x;
+        }
+    }
+
+    if(Xor > 0) cout<<"first"<<endl;
+    else cout<<"second"<<endl;
+
+    return;
+}
+
+int main(){
+    fast;
+    int tc=1;
+    cin >> tc;
+    for(int t=1;t<=tc;t++) an1nd1ta(t);
+    return 0;
+}
+
