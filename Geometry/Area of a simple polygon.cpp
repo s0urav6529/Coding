80c0847c6ba3b60232b4018f03f86e5af2cd61e9
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-12-26 19:39:08 +0600

    Update Area of a simple polygon.cpp

--- Geometry/Area of a simple polygon.cpp
+++ Geometry/Area of a simple polygon.cpp
@@ -2,18 +2,36 @@
 
 ///Given some 2D points of a simple polygone . Find the area of the polygone.
 
-    int n,ans=0,j=1;
-    cin>>n;
-    ll x[n+2],y[n+2];
-    for(int i=1;i<=n;i++) cin>>x[i]>>y[i];
+ll poly_area(vector<pair<ll,ll>>&poly,int n){
 
-    for(int i=1;i<=n;i++){
-        j++;
-        if(i==n) j=1;
- 
-        ans+=((x[i]-x[j])*(y[i]+y[j]));
- 
+    ll area = 0;
+    for(int i=0;i<n;i++){
+        
+        /// Cross product (x1*y2)-(x2*y1)
+        
+        area += (poly[i].first*poly[(i+1)%n].second) - (poly[(i+1)%n].first*poly[i].second);
+
     }
-    cout<<abs(ans)<<endl;
+    area = abs(area);
 
+    return area/2LL;
+
+}
+
+void run_case(){
+
+
+    int n;
+    cin>>n;
+    vector<pair<ll,ll>>poly(n);
+
+    for(int i=0;i<n;i++) {
+        cin>>poly[i].first>>poly[i].second;
+    }
+
+    ll area = poly_area(poly,n);
+
+    cout<<area<<endl;
+    return;
+}
 
