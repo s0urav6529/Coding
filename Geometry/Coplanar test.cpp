85f58feedc5ceee7df1835b21a6d0aa38ff69563
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2022-12-26 19:50:54 +0600

    Create Coplanar test.cpp

---
+++ Geometry/Coplanar test.cpp
@@ -0,0 +1,8 @@
+****************** Coplanar test ************************
+
+Show that 3 points are in the same straight line. A (x1, y1) B (x2, y2) & C (x3, y3).
+They will be coplanar if the area of the triangle bounded by these three points is 0.
+
+Area=x1*(y2-y3) +x2*(y3-y1) +x3*(y1-y2)
+
+If (Area==0) they are coplanar. 
