b002bc7faf5941c220a0a248935ca8e134d54bec
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2023-02-15 22:57:52 +0600

    Add files via upload

---
+++ CSES/Sorting and Searching/Apartments.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define read freopen ("in.txt","r",stdin);
+#define out freopen  ("out.txt","w",stdout);
+#define reset(a,b)   memset(a, (b), sizeof(a))
+#define sortv(k)     sort(k.begin(),k.end())
+#define sortg(k)     sort(k.begin(),k.end(),greater<int>())
+#define uniq(vec)    vec.resize(distance(vec.begin(),unique(vec.begin(),vec.end())))
+#define rev(k)       reverse(k.begin(),k.end())
+#define umin(a,b)    a=min(a,b)
+#define umax(a,b)    a=max(a,b)
+#define min3(a,b,c)  min(a, min(b,c))
+#define pyes         cout<<"Yes"<<endl
+#define pno          cout<<"No"<<endl
+#define pf(a)        cout<<a<<endl
+#define p2f(a,b)     cout<<a<<" "<<b<<endl
+#define p3f(a,b,c)   cout<<a<<" "<<b<<" "<<c<<endl
+#define p4f(a,b,c)   cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl
+#define ff first
+#define ss second
+#define pb push_back
+#define pi acos(-1.0)
+//cin.get();
+typedef long long int ll;
+//typedef unsigned long long int llu;
+//constexpr llu Mx=18446744073709551615;
+constexpr ll MOD=1e9+7;
+const int limit=1e5+5;
+
+void an1nd1ta(int tc){
+    int n , m;
+    ll k;
+    cin >> n >> m >> k;
+    vector<ll>a(n) , b(m);
+    for(int i=0;i<n;i++) cin >> a[i];
+
+    for(int i=0;i<m;i++) cin >> b[i];
+
+    sortv(a);
+	sortv(b);
+
+	if(n>=m){
+		int ans = 0 , j = 0;
+		for(int i = 0 ; i < n && j < m; ){
+
+			if(b[j] >= a[i]-k && b[j] <= a[i] + k){
+				//cout << a[i] <<" "<<b[j] <<endl;
+				ans++;
+				j++;
+				i++;
+			}
+			else {
+				if(a[i] < b[j]) i++;
+				else j++;
+			}
+		}
+
+		cout << ans <<endl;
+	}
+	else{
+
+		int ans = 0 , j = 0;
+		for(int i = 0 ; i < m && j < n; ){
+
+			if(a[j] >= b[i]-k && a[j] <= b[i] + k){
+				//cout << b[i] <<" "<<a[j] <<endl;
+				ans++;
+				j++;
+				i++;
+			}
+			else {
+				if(b[i] < a[j]) i++;
+				else j++;
+			}
+		}
+
+		cout << ans <<endl;
+
+	}
+
+    return;
+}
+
+int main(){
+    fast;
+    int testcase=1;
+    //cin >> testcase;
+    for(int T=1;T<=testcase;T++) an1nd1ta(T);
+    return 0;
+}
---
+++ CSES/Sorting and Searching/Concert_Tickets.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define read freopen ("in.txt","r",stdin);
+#define out freopen  ("out.txt","w",stdout);
+#define reset(a,b)   memset(a, (b), sizeof(a))
+#define sortv(k)     sort(k.begin(),k.end())
+#define sortg(k)     sort(k.begin(),k.end(),greater<int>())
+#define uniq(vec)    vec.resize(distance(vec.begin(),unique(vec.begin(),vec.end())))
+#define rev(k)       reverse(k.begin(),k.end())
+#define umin(a,b)    a=min(a,b)
+#define umax(a,b)    a=max(a,b)
+#define min3(a,b,c)  min(a, min(b,c))
+#define pyes         cout<<"Yes"<<endl
+#define pno          cout<<"No"<<endl
+#define pf(a)        cout<<a<<endl
+#define p2f(a,b)     cout<<a<<" "<<b<<endl
+#define p3f(a,b,c)   cout<<a<<" "<<b<<" "<<c<<endl
+#define p4f(a,b,c)   cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl
+#define ff first
+#define ss second
+#define pb push_back
+#define pi acos(-1.0)
+//cin.get();
+typedef long long int ll;
+//typedef unsigned long long int llu;
+//constexpr llu Mx=18446744073709551615;
+constexpr ll MOD=1e9+7;
+const int limit=1e5+5;
+
+void an1nd1ta(int tc){
+    
+    int n , m;
+    cin >> n >> m;
+    multiset<int>tickets;
+
+    for(int i=0;i<n;i++){
+    	int x;
+    	cin >> x;
+    	tickets.insert(x);
+    }
+
+    for(int i=0;i<m;i++){
+    	int x;
+    	cin >> x;
+
+    	auto it = tickets.upper_bound(x);
+
+    	if(it == tickets.begin()){
+    		cout << -1 <<endl;
+    	}
+    	else{
+    		cout << *(--it) <<endl;
+    		tickets.erase(it);
+    	}
+    	
+    }
+
+    return;
+}
+
+int main(){
+    fast;
+    int testcase=1;
+    //cin >> testcase;
+    for(int T=1;T<=testcase;T++) an1nd1ta(T);
+    return 0;
+}
